cc44613c3687916624e317b0d763f810e08f3cb4
nickmbatia <nickmwangi7322@gmail.com>
2020-02-25 09:10:24 +0300

    add hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,89 @@
-$(document).ready(function() {
-    $("#serv").click(function(){
-        $("#serv-d").show();
-    })
-})
+$(document).ready(function(){
+    //hide and show
+    $("#des-pic").click(function(){
+        $("#des-hidden").hide();
+        $("#des-showing").show();
+    });
+    $("#des-showing").click(function(){
+        $("#des-showing").hide();
+        $("#des-hidden").show();
+    });
+
+    $("#dev-pic").click(function(){
+        $("#dev-hidden").hide();
+        $("#dev-showing").show();
+    });
+    $("#dev-showing").click(function(){
+        $("#dev-showing").hide();
+        $("#dev-hidden").show();
+    });
+
+    $("#prod-pic").click(function(){
+        $("#prod-hidden").hide();
+        $("#prod-showing").show();
+    });
+    $("#prod-showing").click(function(){
+        $("#prod-showing").hide();
+        $("#prod-hidden").show();
+    });
+    
+
+    //hover1
+    $("#proj1").hover(function(){
+        $(this).animate({opacity: '0.5'});
+    }, 
+    function() {
+        $(this).animate({opacity: '1'})
+    });
+    //hover2
+    $("#proj2").hover(function(){
+        $(this).animate({opacity: '0.5'});
+    }, 
+    function() {
+        $(this).animate({opacity: '1'})
+    });
+    //hover3
+    $("#proj3").hover(function(){
+        $(this).animate({opacity: '0.5'});
+    }, 
+    function() {
+        $(this).animate({opacity: '1'})
+    });
+    //hover4
+    $("#proj4").hover(function(){
+        $(this).animate({opacity: '0.5'});
+    }, 
+    function() {
+        $(this).animate({opacity: '1'})
+    });
+    //hover5
+    $("#proj5").hover(function(){
+        $(this).animate({opacity: '0.5'});
+    }, 
+    function() {
+        $(this).animate({opacity: '1'})
+    });
+    //hover6
+    $("#proj6").hover(function(){
+        $(this).animate({opacity: '0.5'});
+    }, 
+    function() {
+        $(this).animate({opacity: '1'})
+    });
+    //hover7
+    $("#proj7").hover(function(){
+        $(this).animate({opacity: '0.5'});
+    }, 
+    function() {
+        $(this).animate({opacity: '1'})
+    });
+    //hover8
+    $("#proj8").hover(function(){
+        $(this).animate({opacity: '0.5'});
+    }, 
+    function() {
+        $(this).animate({opacity: '1'})
+    });
+
+
+});
